10044fe966dc787fceb6d9d58e14774b9db8da8d
김채은 <jinnie313@naver.com>
2019-10-17 16:53:08 +0900

    191017|command crawling

--- backend/backend/crawling.py
+++ backend/backend/crawling.py
@@ -1,20 +1,50 @@
 import requests
+import math
 from bs4 import BeautifulSoup
 
 
-# test_url="http://www.bokjiro.go.kr/welInfo/retrieveWelInfoDetail.do?searchIntClId=01&searchCnDivCd=&welInfSno=315&searchGb=01&searchText=&searchSidoCode=&searchCggCode=&searchCtgId=999&pageGb=1&pageUnit=10&pageIndex=1&domainName=&cardListTypeCd=list&welSrvTypeCd=01&age=&hirkQestId=&qestCric=&qestDsr=&searchCondition=&searchKeyword=&intClId=&region1=&region2=&occupation=&occupation4=&pref=&career=&education=&regDate=&searchGbn=&hdHelpLen=1&hdWelHelpId=div_doc_5594691&hdWelHelpNm=%EC%88%98%EA%B8%89%EA%B6%8C%EC%9E%90&hdWelHelpCn=%EC%82%AC%ED%9A%8C%EB%B3%B5%EC%A7%80%EC%82%AC%EC%97%85+%EA%B4%80%EB%A0%A8%EB%B2%95%EC%97%90+%EC%9D%98%ED%95%9C+%EA%B8%89%EC%97%AC+%EB%98%90%EB%8A%94+%EC%84%9C%EB%B9%84%EC%8A%A4%EB%A5%BC+%EB%B0%9B%EC%9D%84+%EC%9E%90%EA%B2%A9%EC%9D%B4+%EC%9E%88%EB%8A%94+%EC%9E%90&hdChcNum=1&key1=315&stsfCn="
-# test_url = "http://www.bokjiro.go.kr/welInfo/retrieveWelInfoDetail.do?welInfSno=95"
-test_url = "http://www.bokjiro.go.kr/welInfo/retrieveWelInfoDetail.do?welInfSno=315"
+def getList(url):
+    resp = requests.get(url)
+    html = BeautifulSoup(resp.content, 'html.parser')
+    lis = html.findAll('dt', {'class': 'tit'})
+
+    print("{}페이지// url:{}".format(page, url))
+    for li in lis:
+        codeNum = li.find('a').get('href')[20:-3]
+        getService("http://www.bokjiro.go.kr/welInfo/retrieveGvmtWelInfo.do?searchIntClId=&welInfSno={}".format(codeNum))
+        # print(codeNum)
+
+def getService(url):
+    print(url)
+
+
+
+# test_url = "http://www.bokjiro.go.kr/welInfo/retrieveWelInfoDetail.do?welInfSno=95"    #상세보기
+test_url="http://www.bokjiro.go.kr/welInfo/retrieveLcgWelInfoList.do?searchSidoCode=&searchCggCode=&searchIntClId=&searchCnDivCd=&searchCtgId=&searchGb=&searchText=&pageUnit=10"
 resp = requests.get(test_url)
 html = BeautifulSoup(resp.content, 'html.parser')
 print(html)
-# #댓글 전체 수
-pageTotal= html.find('div', {'class':'score_total'}).find('strong').findChildren('em')[1].getText()
-pageTotal = int(pageTotal.replace(',', ''))
 
-for i in range(1, 101):
-    url = test_url + '&page=' + str(i)
-    print(url)
-    # get_data(url)
+#댓글 전체 수
+commandTotal= int(html.find('strong', {'class':'point01'}).getText().replace(',',''))
+#페이지 수
+pages = math.floor(commandTotal/10)
+
+page = 2
+url = test_url + '&pageIndex=' + str(page)
+getList(url)
+
+# # 나머지 뺀 페이지
+# for page in range(1, pages+1):
+#     url = test_url + '&pageIndex=' + str(page)
+#     getList(url)
+#
+# # 나머지 부분
+# page = pages+1
+# nmg = commandTotal-pages*10
+# if nmg != 0:
+#     url = test_url + '&pageIndex=' + str(page)
+#     getList(url)
+
 
 
